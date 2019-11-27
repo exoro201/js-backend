969dbb49b8dbeb1d58365a66d722b7a684d1b70d
students <students@stepdn.com>
2019-11-27 18:30:51 +0300

    five

---
+++ src/script.js
@@ -0,0 +1,31 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () =>{
+    const task  = [
+        {
+            id: '1',
+            title: 'New Task',
+            body: 'Some Text For Task',
+        },
+        {
+            id: '2',
+            title: 'New Task 2',
+            body: 'Some Text For Task',
+        },
+        {
+            id: '3',
+            title: 'New Task 3',
+            body: 'Some Text For Task',
+        }
+    ];
+
+    const form = document.forms[ 'taskForm'];
+    const inputTitle = form[0];
+    const inputBody = form[1];
+
+
+
+
+
+
+
+});
